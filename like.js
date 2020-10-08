2f2d80a6bffa393cb0902e5616ca0848671430b3
Towerbank <zunderglast@gmail.com>
2020-10-08 09:32:25 +0300

    08.10

---
+++ like.js
@@ -0,0 +1,11 @@
+let like = document.querySelector('.article__like');
+let likesNumber = document.querySelector('.article__likes-number')
+
+like.onclick = function () {
+    if (like.classList.contains('added')) {
+        likesNumber.textContent--;
+    } else {
+        likesNumber.textContent++;
+    }
+    like.classList.toggle('added');
+};
